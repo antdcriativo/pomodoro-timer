a5b187551d3f3ad0aadfa4c5bb1b850fb4419393
antdcriativo <antonielcz@gmail.com>
2022-06-25 18:15:55 -0300

    tirada-do-iniciar

--- script.js
+++ script.js
@@ -37,112 +37,111 @@ function iniciar() {
 
         momentoAcao()
     }
+}
 
-    function momentoAcao() {
+function momentoAcao() {
 
-        let sessoes_valor = localStorage.getItem('sessoes')
+    let sessoes_valor = localStorage.getItem('sessoes')
 
-        if (sessoes_valor != '1') {
-            document.getElementById('title_sessao').innerHTML = sessoes_valor + ' sessões restantes'
-        } else {
-            document.getElementById('title_sessao').innerHTML = sessoes_valor + ' sessão restante'
-        }
-
-        let title = document.getElementById('title')
-        title.innerHTML = "AÇÃO"
-        title.style.fontSize = '25pt'
-        title.style.fontWeight = 'bold'
-        title.style.setProperty('color', '#28a745', 'important')
-
-        min = Number(localStorage.getItem('acao'))
-
-        min = min - 1
-        segundos = 59
-
-        document.getElementById('minutos_ok').innerHTML = min
-        document.getElementById('seconds_ok').innerHTML = segundos
-
-        var min_interval = setInterval(minTimer, 60000)
-        var seg_interval = setInterval(segTimer, 1000)
-
-        function minTimer() {
-            min = min - 1
-            document.getElementById('minutos_ok').innerHTML = min
-        }
-
-        function segTimer() {
-            segundos = segundos - 1
-            document.getElementById('seconds_ok').innerHTML = segundos
-
-            if (segundos <= 0) {
-                if (min <= 0) {
-                    clearInterval(min_interval)
-                    clearInterval(seg_interval)
-
-                    bell.play()
-
-                    momentoPausa()
-                }
-
-                segundos = 60
-            }
-        }
+    if (sessoes_valor != '1') {
+        document.getElementById('title_sessao').innerHTML = sessoes_valor + ' sessões restantes'
+    } else {
+        document.getElementById('title_sessao').innerHTML = sessoes_valor + ' sessão restante'
     }
 
-    function momentoPausa() {
-        let title = document.getElementById('title')
-        title.innerHTML = "PAUSA"
-        title.style.fontSize = '25pt'
-        title.style.fontWeight = 'bold'
-        title.style.setProperty('color', '#dc3545', 'important')
+    let title = document.getElementById('title')
+    title.innerHTML = "AÇÃO"
+    title.style.fontSize = '25pt'
+    title.style.fontWeight = 'bold'
+    title.style.setProperty('color', '#28a745', 'important')
 
-        min_pausa = Number(localStorage.getItem('pausa'))
+    min = Number(localStorage.getItem('acao'))
 
-        min_pausa = min_pausa - 1
-        segundos = 59
+    min = min - 1
+    segundos = 59
 
-        document.getElementById('minutos_ok').innerHTML = min_pausa
+    document.getElementById('minutos_ok').innerHTML = min
+    document.getElementById('seconds_ok').innerHTML = segundos
+
+    var min_interval = setInterval(minTimer, 60000)
+    var seg_interval = setInterval(segTimer, 1000)
+
+    function minTimer() {
+        min = min - 1
+        document.getElementById('minutos_ok').innerHTML = min
+    }
+
+    function segTimer() {
+        segundos = segundos - 1
         document.getElementById('seconds_ok').innerHTML = segundos
 
-        var min_interval = setInterval(minTimer, 60000)
-        var seg_interval = setInterval(segTimer, 1000)
+        if (segundos <= 0) {
+            if (min <= 0) {
+                clearInterval(min_interval)
+                clearInterval(seg_interval)
 
-        function minTimer() {
-            min_pausa = min_pausa - 1
-            document.getElementById('minutos_ok').innerHTML = min_pausa
+                bell.play()
+
+                momentoPausa()
+            }
+
+            segundos = 60
         }
+    }
+}
 
-        function segTimer() {
-            segundos = segundos - 1
-            document.getElementById('segundos_ok').innerHTML = segundos
+function momentoPausa() {
+    let title = document.getElementById('title')
+    title.innerHTML = "PAUSA"
+    title.style.fontSize = '25pt'
+    title.style.fontWeight = 'bold'
+    title.style.setProperty('color', '#dc3545', 'important')
 
-            if (segundos <= 0) {
-                if (min_pausa <= 0) {
-                    ses = Number(localStorage.getItem('sessoes'))
-                    ses = ses - 1
-                    localStorage.setItem('sessoes', String(ses))
+    min_pausa = Number(localStorage.getItem('pausa'))
 
-                    clearInterval(min_interval)
-                    clearInterval(seg_interval)
+    min_pausa = min_pausa - 1
+    segundos = 59
 
-                    if (ses <= 0) {
-                        final.play()
-                        localStorage.clear()
-                        document.getElementById('config').style.setProperty('display', 'none', 'important')
-                        document.getElementById('timer').style.setProperty('display', 'none', 'important')
-                        document.getElementById('fim').style.setProperty('display', 'block', 'important')
-                    } else {
-                        volta.play()
+    document.getElementById('minutos_ok').innerHTML = min_pausa
+    document.getElementById('seconds_ok').innerHTML = segundos
 
-                        momentoAcao()
-                    }
+    var min_interval = setInterval(minTimer, 60000)
+    var seg_interval = setInterval(segTimer, 1000)
 
+    function minTimer() {
+        min_pausa = min_pausa - 1
+        document.getElementById('minutos_ok').innerHTML = min_pausa
+    }
+
+    function segTimer() {
+        segundos = segundos - 1
+        document.getElementById('segundos_ok').innerHTML = segundos
+
+        if (segundos <= 0) {
+            if (min_pausa <= 0) {
+                ses = Number(localStorage.getItem('sessoes'))
+                ses = ses - 1
+                localStorage.setItem('sessoes', String(ses))
+
+                clearInterval(min_interval)
+                clearInterval(seg_interval)
+
+                if (ses <= 0) {
+                    final.play()
+                    localStorage.clear()
+                    document.getElementById('config').style.setProperty('display', 'none', 'important')
+                    document.getElementById('timer').style.setProperty('display', 'none', 'important')
+                    document.getElementById('fim').style.setProperty('display', 'block', 'important')
+                } else {
+                    volta.play()
+
+                    momentoAcao()
                 }
 
-                segundos = 60
             }
-        }
 
+            segundos = 60
+        }
     }
 
 }
