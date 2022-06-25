211f18d183e73359763e67e6392ea8cd5e8de5ca
antdcriativo <antonielcz@gmail.com>
2022-06-25 14:33:51 -0300

    script-function

---
+++ script.js
@@ -0,0 +1,30 @@
+let acao = document.getElementById('acao')
+let pausa = document.getElementById('pausa')
+let sessoes = document.getElementById('sessoes')
+let segundos
+
+var bell = new Audio("./audio/bell.mp3")
+var volta = new Audio("./audio/volta.mp3")
+var final = new Audio("./audio/final.mp3")
+
+var lofi = document.getElementById('lofi')
+var pause = document.getElementById('pause')
+var play = document.getElementById('play')
+
+function iniciar(){
+    if (acao.value == 0){
+        document.getElementById('erro_acao').innerHTML= "Adicione os minutos"
+        acao.focus()
+    } else if(pausa.value == 0){
+        document.getElementById('erro_pausa').innerHTML= "Adicione a pausa"
+        pausa.focus()
+    } else if(sessao.value == 0){
+        document.getElementById('erro_sessao').innerHTML= "Adicione as sessões"
+        sessoes.focus()
+    }
+    else {
+        lofi.play()
+        pause.style.setProperty('display', 'block', 'important')
+    }
+
+}
